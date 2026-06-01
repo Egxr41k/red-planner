733e5da8f0d07bcad803ea4003a6c4e89c793a9b
egxr41k <egor2005krava@gmail.com>
2026-06-01 16:25:02 +0300

    fix debian script

--- infra/vms/deploy_debian_nodes.sh
+++ infra/vms/deploy_debian_nodes.sh
@@ -1,61 +1,68 @@
 #!/bin/bash
 
+# Подгружаем пути к образам, если они в отдельном файле
+# source ./install_imgs.sh 
+
 BASE_IMG="./debian-12-generic-amd64.qcow2"
 LIBVIRT_DIR="/var/lib/libvirt/images"
-K8S_SCRIPT="./k8s_setup.sh" # Путь к вашему скрипту для нод
+K8S_SCRIPT="./k8s_setup.sh"
 
 for i in {0..5}; do
     NAME="debian-node$i"
     echo "--- Развертывание $NAME ---"
 
     # 1. Копируем чистый образ
-    sudo cp $BASE_IMG $LIBVIRT_DIR/$NAME.qcow2
+    sudo cp "$BASE_IMG" "$LIBVIRT_DIR/$NAME.qcow2"
 
-    # 1. Генерируем meta-data
-    echo "instance-id: $NAME-id" > meta-data
-    echo "local-hostname: $NAME" >> meta-data
+    # 2. УВЕЛИЧИВАЕМ образ до 40 ГБ (cloud-init сам расширит файловую систему внутри)
+    sudo qemu-img resize "$LIBVIRT_DIR/$NAME.qcow2" 40G
 
-    # 2. Генерируем user-data
+    # 3. Генерируем meta-data
+    cat <<EOF > meta-data
+instance-id: $NAME-id
+local-hostname: $NAME
+EOF
+
+    # 4. Генерируем user-data
+    # Используем > чтобы файл всегда был свежим
     cat <<EOF > user-data
 #cloud-config
 user: user
 password: user
 chpasswd: { expire: False }
 ssh_pwauth: True
+sudo: ALL=(ALL) NOPASSWD:ALL
 
 # Копируем скрипт внутрь виртуалки
 write_files:
   - path: /usr/local/bin/k8s_setup.sh
     permissions: '0755'
     content: |
-$(sed 's/^/      /' $K8S_SCRIPT)
+$(sed 's/^/      /' "$K8S_SCRIPT")
 
 # Запускаем скрипт при первой загрузке
 runcmd:
   - /usr/local/bin/k8s_setup.sh > /var/log/k8s_setup.log 2>&1
 EOF
 
-    # 3. Создаем seed.iso
-    cloud-localds $NAME-seed.iso user-data meta-data
-    sudo mv $NAME-seed.iso $LIBVIRT_DIR/$NAME-seed.iso
+    # 5. Создаем seed.iso
+    cloud-localds "${NAME}-seed.iso" user-data meta-data
+    sudo mv "${NAME}-seed.iso" "$LIBVIRT_DIR/${NAME}-seed.iso"
     rm meta-data user-data
 
-    # 4. Запускаем виртуалку
+    # 6. Запускаем виртуалку
     sudo virt-install \
-      --name $NAME \
+      --name "$NAME" \
       --memory 2048 \
       --vcpus 2 \
-      --disk path=$LIBVIRT_DIR/$NAME.qcow2,format=qcow2 \
-      --disk path=$LIBVIRT_DIR/$NAME-seed.iso,device=cdrom \
+      --disk path="$LIBVIRT_DIR/$NAME.qcow2,format=qcow2" \
+      --disk path="$LIBVIRT_DIR/${NAME}-seed.iso,device=cdrom" \
       --os-variant debian12 \
       --network network=default \
       --import \
       --noautoconsole
 
-    echo "$NAME запущен и настраивается!"
+    echo "$NAME запущен, диск 40ГБ, идет настройка k8s..."
 done
 
 sudo virsh list --all
-
-echo "--- Текущие IP адреса в сети ---"
-sudo virsh net-dhcp-leases default
